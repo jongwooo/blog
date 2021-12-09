0003244bfe8d7942a98804432578a5b0b29b195d
jongwooo <han980817@gmail.com>
2021-12-10 04:17:22 +0900

    chore: Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,37 @@
-exports.createPages = async ({ actions }) => {
+exports.createPages = async ({ actions, graphql }) => {
     const { createPage } = actions
-    createPage({
-        path: '/using-dsg',
-        component: require.resolve('./src/templates/using-dsg.js'),
-        context: {},
-        defer: true,
-    })
+
+    const { data, errors } = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            frontmatter {
+              date
+              title
+              description
+              path 
+            }
+          }
+        }
+      }
+    }
+  `);
+
+    if (errors) {
+        throw errors;
+    }
+
+    data['allMarkdownRemark'].edges.forEach(({ node }) => {
+        createPage({
+            path: node.frontmatter.path,
+            context: {
+                html: node.html,
+                title: node.frontmatter.title,
+                description: node.frontmatter.description
+            },
+            component: require.resolve('./src/templates/postTemplate.js'),
+        });
+    });
 }
