bf5485c70a3e8874b3234f8be8a91ebb7dc18be8
jongwooo <han980817@gmail.com>
2021-12-10 02:39:22 +0900

    chore: Add gatsby-transformer-remark to gatsby-config.js as plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/posts`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
