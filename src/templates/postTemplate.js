ac86064cce5dff3eab7f883b2ba6541ac436da9c
jongwooo <han980817@gmail.com>
2021-12-10 04:16:22 +0900

    feat: Create postTemplate.js

---
+++ src/templates/postTemplate.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Layout from '../layout';
+import Seo from "../components/seo";
+
+const PostTemplate = React.memo(props => {
+    const { title, date, html } = props['pageContext'];
+    return (
+        <Layout>
+            <Seo title={title} />
+            <h2>{title}</h2>
+            <h4>{date}</h4>
+            <hr />
+            <div dangerouslySetInnerHTML={{ __html: html }} />
+        </Layout>
+    );
+});
+
+PostTemplate.displayName = 'PostTemplate';
+
+export default PostTemplate;
