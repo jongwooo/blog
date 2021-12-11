6a7f04222ba81a2e1fcb20cfcf5956a6b2541274
jongwooo <han980817@gmail.com>
2021-12-12 05:19:14 +0900

    feat: Update template: PostTemplate

--- src/templates/postTemplate.js
+++ src/templates/postTemplate.js
@@ -1,20 +1,61 @@
-import React from "react"
+import * as React from "react"
+import { graphql } from "gatsby"
+
+import Post from "../models/post"
 import Layout from "../layout"
 import Seo from "../components/seo"
+import PostNavigator from "../components/post-navigator"
 
-const PostTemplate = React.memo(props => {
-    const { title, date, html } = props["pageContext"]
+const PostTemplate = ({ data }) => {
+    const currentPost = new Post(data.current)
+    const previousPost = data.previous && new Post(data.previous)
+    const nextPost = data.next && new Post(data.next)
+
     return (
         <Layout>
-            <Seo title={title} />
-            <h2>{title}</h2>
-            <h4>{date}</h4>
+            <Seo title={currentPost?.title} description={currentPost?.description} />
+            <h2>{currentPost.title}</h2>
+            <p>{currentPost.date}</p>
             <hr />
-            <div dangerouslySetInnerHTML={{ __html: html }} />
+            <div dangerouslySetInnerHTML={{ __html: currentPost.html }} />
+            <PostNavigator previousPost={previousPost} nextPost={nextPost} />
         </Layout>
     )
-})
-
-PostTemplate.displayName = "PostTemplate"
+}
 
 export default PostTemplate
+
+export const pageQuery = graphql`
+    query ($slug: String, $previousSlug: String, $nextSlug: String) {
+        current: markdownRemark(fields: { slug: { eq: $slug } }) {
+            id
+            html
+            frontmatter {
+                date(formatString: "YYYY년 MM월 DD일")
+                title
+                description
+            }
+            fields {
+                slug
+            }
+        }
+        previous: markdownRemark(fields: { slug: { eq: $previousSlug } }) {
+            id
+            frontmatter {
+                title
+            }
+            fields {
+                slug
+            }
+        }
+        next: markdownRemark(fields: { slug: { eq: $nextSlug } }) {
+            id
+            frontmatter {
+                title
+            }
+            fields {
+                slug
+            }
+        }
+    }
+`
