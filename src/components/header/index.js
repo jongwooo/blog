f9de43233b27dee79f7e7fbfde19ff9552709507
jongwooo <han980817@gmail.com>
2021-12-15 23:35:43 +0900

    feat: Update the Header to add style

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,42 +1,15 @@
 import * as React from "react"
-import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import "./style.scss"
 
 const Header = ({ siteTitle }) => (
-    <header
-        style={{
-            background: `rebeccapurple`,
-            marginBottom: `1.45rem`,
-        }}
-    >
-        <div
-            style={{
-                margin: `0 auto`,
-                maxWidth: 960,
-                padding: `1.45rem 1.0875rem`,
-            }}
-        >
-            <h1 style={{ margin: 0 }}>
-                <Link
-                    to="/"
-                    style={{
-                        color: `white`,
-                        textDecoration: `none`,
-                    }}
-                >
-                    {siteTitle}
-                </Link>
-            </h1>
+    <header className="header-wrapper">
+        <div className="header">
+            <Link to="/" className="header-title">
+                { siteTitle }
+            </Link>
         </div>
     </header>
 )
-
-Header.propTypes = {
-    siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-    siteTitle: ``,
-}
 
 export default Header
