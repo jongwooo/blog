56d84820683cd844ba201186940940d84f550f0d
jongwooo <han980817@gmail.com>
2021-12-16 13:19:27 +0900

    feat: Define the size of mobile and desktop screens

--- src/components/post-card/index.jsx
+++ src/components/post-card/index.jsx
@@ -1,15 +1,16 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
+import "./style.scss"
+
 const PostCard = ({ post }) => {
     return (
-        <li key={post.id}>
-            <h2>
-                <Link to={post.slug}>{post.title}</Link>
-            </h2>
-            <p>{post.date}</p>
-            <p>{post.excerpt}</p>
-            <hr />
+        <li className="post-card" key={post.id}>
+            <Link to={post.slug}>
+                <h4>{post.title}</h4>
+                <p className="post-card-description">{post.description}</p>
+                <p className="post-card-date">{post.date}</p>
+            </Link>
         </li>
     )
 }
