7b96aa1701a246850f0a5506f86fd219e464356a
jongwooo <han980817@gmail.com>
2021-12-16 13:01:26 +0900

    refactor: Change file extension from .js to .jsx format

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -6,12 +6,10 @@
  */
 
 import * as React from "react"
-import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "../components/header"
-import Footer from "../components/footer"
-import "./index.css"
+import "./style.scss"
 
 const Layout = ({ children }) => {
     const data = useStaticQuery(graphql`
@@ -27,22 +25,11 @@ const Layout = ({ children }) => {
     return (
         <>
             <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-            <div
-                style={{
-                    margin: `0 auto`,
-                    maxWidth: 960,
-                    padding: `0 1.0875rem 1.45rem`,
-                }}
-            >
+            <div className="content-wrapper">
                 <main>{children}</main>
-                <Footer />
             </div>
         </>
     )
-}
-
-Layout.propTypes = {
-    children: PropTypes.node.isRequired,
 }
 
 export default Layout
