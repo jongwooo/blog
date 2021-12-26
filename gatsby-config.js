2fbd83adeee4baa3c76e55d55fc88fce5678a3ab
jongwooo <han980817@gmail.com>
2021-12-26 20:00:27 +0900

    chore: Add gatsby-transformer-images to yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,6 @@ module.exports = {
                 path: `${__dirname}/posts`,
             },
         },
-        `gatsby-transformer-remark`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {
@@ -39,6 +38,22 @@ module.exports = {
                 // theme_color: `#663399`,
                 display: `minimal-ui`,
                 icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            // It's important to specify the maxWidth (in pixels) of
+                            // the content container as this plugin uses this as the
+                            // base for generating different widths of each image.
+                            maxWidth: 590,
+                        },
+                    },
+                ],
             },
         },
         // this (optional) plugin enables Progressive Web App + Offline functionality
