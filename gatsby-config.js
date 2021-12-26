6e776314781575f3d93ec1a69fef5f522ef1ceeb
jongwooo <han980817@gmail.com>
2021-12-26 23:36:40 +0900

    chore: Update gatsby-config.js to apply favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
     siteMetadata: {
         title: `아카이브-로그`,
         author: `한종우`,
-        description: `프론트엔드 엔지니어 한종우입니다`,
+        description: `프론트엔드 엔지니어 한종우입니다.`,
         siteUrl: `https://thearchivelog.dev/`,
         githubUrl: `https://github.com/jongwooo/`,
     },
@@ -32,12 +32,8 @@ module.exports = {
                 name: `gatsby-starter-default`,
                 short_name: `starter`,
                 start_url: `/`,
-                background_color: `#663399`,
-                // This will impact how browsers show your PWA/website
-                // https://css-tricks.com/meta-theme-color-and-trickery/
-                // theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+                icon: `src/images/favicon-32x32.png`,
             },
         },
         {
@@ -47,17 +43,11 @@ module.exports = {
                     {
                         resolve: `gatsby-remark-images`,
                         options: {
-                            // It's important to specify the maxWidth (in pixels) of
-                            // the content container as this plugin uses this as the
-                            // base for generating different widths of each image.
                             maxWidth: 590,
                         },
                     },
                 ],
             },
         },
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
     ],
 }
