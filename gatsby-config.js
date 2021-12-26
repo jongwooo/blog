a58ca9a9d07813ea4485f2dd34072ce1b30835e1
jongwooo <han980817@gmail.com>
2021-12-26 20:03:31 +0900

    chore: Remove gatsby-plugin-sass from gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-sass`,
         `gatsby-plugin-styled-components`,
         `gatsby-plugin-image`,
         {
