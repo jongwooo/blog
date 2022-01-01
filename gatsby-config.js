3ef60bd7c4e784cc4172202502d613f61d5b9a8b
jongwooo <han980817@gmail.com>
2022-01-02 00:12:58 +0900

    chore: Update gatsby-config.js to add captions to the image

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,8 @@ module.exports = {
                         resolve: `gatsby-remark-images`,
                         options: {
                             maxWidth: 590,
+                            showCaptions: ["title"],
+                            markdownCaptions: true,
                         },
                     },
                 ],
