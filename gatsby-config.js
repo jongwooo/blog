58e85d52bf69fc687883010eec5b8772ab2de4da
jongwooo <han980817@gmail.com>
2022-01-08 15:45:23 +0900

    chore: Separate site metadata into gatsby-meta-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,8 @@
+const metaConfig = require("./gatsby-meta-config")
+
 module.exports = {
-    siteMetadata: {
-        title: `아카이브-로그`,
-        author: `한종우`,
-        description: `프론트엔드 엔지니어 한종우입니다.`,
-        siteUrl: `https://thearchivelog.dev/`,
-        githubUrl: `https://github.com/jongwooo/`,
-        repo: `jongwooo/blog`,
-    },
+    siteMetadata: metaConfig,
+
     plugins: [
         {
             resolve: `gatsby-plugin-gtag`,
---
+++ gatsby-meta-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: `아카이브-로그`,
+    author: `한종우`,
+    description: `프론트엔드 엔지니어 한종우입니다.`,
+    siteUrl: `https://thearchivelog.dev/`,
+    githubUrl: `https://github.com/jongwooo/`,
+    repo: `jongwooo/blog`,
+}
