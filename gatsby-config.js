43bf8562ceca1a54cd3c0bb1948b026f2aa6f2f4
jongwooo <han980817@gmail.com>
2022-01-09 18:38:19 +0900

    chore: Add gatsby-plugin-feed to gatsby-config.js as plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,59 @@ module.exports = {
 
     plugins: [
         {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+                query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allMarkdownRemark } }) => {
+                            return allMarkdownRemark.edges.map(edge => {
+                                return Object.assign({}, edge.node.frontmatter, {
+                                    description: edge.node.description,
+                                    date: edge.node.frontmatter.date,
+                                    url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                    guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                    custom_elements: [{ "content:encoded": edge.node.html }],
+                                })
+                            })
+                        },
+                        query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      html
+                      fields { slug }
+                      frontmatter {
+                        date
+                        title
+                        description
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+                        output: `/rss.xml`,
+                        title: `${metaConfig.title}`,
+                    },
+                ],
+            },
+        },
+        {
             resolve: `gatsby-plugin-gtag`,
             options: {
                 trackingId: `G-G347H7GN1W`,
