0fd437ed6d527ebb669cac1a402f6ded34103b49
jongwooo <han980817@gmail.com>
2022-01-10 01:50:48 +0900

    feat: Add excerpt to the model

--- src/models/post.js
+++ src/models/post.js
@@ -1,11 +1,12 @@
 export default class Post {
     constructor(node) {
-        const { id, html, frontmatter, fields } = node
+        const { id, html, excerpt, frontmatter, fields } = node
         const { slug } = fields
-        const { title, description, date } = frontmatter
+        const { title, date, description } = frontmatter
 
         this.id = id
         this.html = html
+        this.excerpt = excerpt
         this.slug = slug
         this.title = title
         this.date = date
