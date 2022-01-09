af6aac0ed540cfaa002893c39698e3017d410f6b
jongwooo <han980817@gmail.com>
2022-01-10 00:35:30 +0900

    chore: Add a greeting to the site metadata

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,7 +1,8 @@
 module.exports = {
     title: `아카이브-로그`,
     author: `한종우`,
-    description: `프론트엔드 엔지니어 한종우입니다.`,
+    greetings: `프론트엔드 엔지니어 한종우입니다.`,
+    description: `한종우의 기술 블로그입니다. 개발하면서 얻은 지식과 경험을 공유합니다.`,
     siteUrl: `https://thearchivelog.dev`,
     githubUrl: `https://github.com/jongwooo`,
     repo: `jongwooo/blog`,
