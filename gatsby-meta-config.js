eb5d81bba858def9da11ef00b18aaa01f206e557
jongwooo <han980817@gmail.com>
2022-01-09 18:38:26 +0900

    fix: Remove duplicates slashes from the path

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: `아카이브-로그`,
     author: `한종우`,
     description: `프론트엔드 엔지니어 한종우입니다.`,
-    siteUrl: `https://thearchivelog.dev/`,
-    githubUrl: `https://github.com/jongwooo/`,
+    siteUrl: `https://thearchivelog.dev`,
+    githubUrl: `https://github.com/jongwooo`,
     repo: `jongwooo/blog`,
 }
