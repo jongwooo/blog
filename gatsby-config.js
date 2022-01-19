c1250d2c19f9429f915f1c47d8fea9dcc15c7fe3
jongwooo <han980817@gmail.com>
2022-01-20 02:17:07 +0900

    chore: Add a separator for excerpt

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,6 +114,7 @@ module.exports = {
         {
             resolve: `gatsby-transformer-remark`,
             options: {
+                excerpt_separator: `<!-- end -->`,
                 plugins: [
                     {
                         resolve: `gatsby-remark-images`,
