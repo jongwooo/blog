cc1b9a9c7db9c347cd33ab49603c579e9806f210
Jongwoo Han <han980817@gmail.com>
2022-06-30 03:41:50 +0900

    chore: Add gatsby-remark-prismjs to gatsby-config.js as plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -124,6 +124,16 @@ module.exports = {
                             markdownCaptions: true,
                         },
                     },
+                    {
+                        resolve: `gatsby-remark-prismjs`,
+                        options: {
+                            classPrefix: "language-",
+                            inlineCodeMarker: null,
+                            aliases: {},
+                            showLineNumbers: false,
+                            noInlineHighlight: false,
+                        },
+                    },
                 ],
             },
         },
