376a177bfb574191ea953737c014df107769baf3
jongwooo <han980817@gmail.com>
2022-10-15 20:47:34 +0900

    chore: Update site meta configurations

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,9 +1,9 @@
 module.exports = {
     siteTitle: `아카이브-로그`,
     author: `한종우`,
-    greetings: `프론트엔드 엔지니어 한종우입니다.`,
-    siteDescription: `프론트엔드 엔지니어 한종우의 기술 블로그입니다.`,
-    siteKeywords: `블로그, 기술 블로그, 아카이브-로그, 프론트엔드, 개발자`,
+    greetings: `소프트웨어 엔지니어 한종우입니다.`,
+    siteDescription: `소프트웨어 엔지니어 한종우의 기술 블로그입니다.`,
+    siteKeywords: `블로그, 기술 블로그, 아카이브-로그, 개발자`,
     defaultOgImage: `/default-og-image.png`,
     siteUrl: `https://thearchivelog.dev`,
     githubUrl: `https://github.com/jongwooo`,
