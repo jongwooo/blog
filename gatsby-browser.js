fd0ef3fe50fb9772082ece98a06292f6d13f79b0
jongwooo <jongwooo.han@gmail.com>
2023-01-18 01:06:27 +0900

    chore: Use syntax highlighter theme skeleton

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,1 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
-import "@jongwooo/prism-theme-github/themes/prism-github-default-light.min.css";
+import "@jongwooo/prism-theme-github/themes/prism-github-skeleton.min.css";
